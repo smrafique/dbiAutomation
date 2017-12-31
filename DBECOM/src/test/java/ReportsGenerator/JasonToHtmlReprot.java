2d4bdff0ee8d665481d74bdf469ba128f45731e2
smrafique <34661823+smrafique@users.noreply.github.com>
2017-12-31 10:13:03 -0500

    Add files via upload

--- DBECOM/src/test/java/ReportsGenerator/JasonToHtmlReprot.java
+++ DBECOM/src/test/java/ReportsGenerator/JasonToHtmlReprot.java
@@ -27,34 +27,41 @@ public class JasonToHtmlReprot {
         JSONArray a = (JSONArray) parser.parse(new FileReader("target/Reports/cucumber.json"));
 
 
-        for (Object o : a)
+        for (int o=0; o<a.size();o++)
         {
-            JSONObject person = (JSONObject) o;
-            htmlTestReport.add("<td>"+ person.get("name")+"</td>");
-            JSONArray elements = (JSONArray) person.get("elements");
+            JSONObject reportFile = (JSONObject) a.get(o);
+            htmlTestReport.add("<td style=\"font-weight: bold; color: #0000ff\";>"+(o+1)+" :"+ reportFile.get("name")+"</td>");
+            JSONArray elements = (JSONArray) reportFile.get("elements");
 
             for (int j=0;j<elements.size();j++) {
-                JSONObject car = (JSONObject) elements.get(j);
+                JSONObject feature = (JSONObject) elements.get(j);
 
                 if(j==0){
-                    htmlTestReport.add("<td>" + car.get("name") + "</td>");
+                    htmlTestReport.add("<td style=\"font-weight: bold;color: #ffa500\";>" +(j+1)+" :"+ feature.get("name") + "</td>");
                 }else {
-                    htmlTestReport.add("<td></td><td>" + car.get("name") + "</td>");
+                    htmlTestReport.add("<td></td><td style=\"font-weight: bold; color: #ffa500\";>" +(j+1)+" :"+ feature.get("name") + "</td>");
                 }
-                JSONArray steps = (JSONArray) car.get("steps");
+                JSONArray steps = (JSONArray) feature.get("steps");
                 for (int i=0; i<steps.size();i++){
-                    JSONObject bar = (JSONObject) steps.get(i);
-                    JSONObject status =(JSONObject) bar.get("result");
+                    JSONObject stepDetails = (JSONObject) steps.get(i);
+                    JSONObject status =(JSONObject) stepDetails.get("result");
                     if(i==0){
-                        htmlTestReport.add("<td>" + bar.get("keyword") + "</td><td>" + bar.get("name") + "</td><td ");
+                        htmlTestReport.add("<td>"+(i+1)+"</td><td style=\" color: #4CAF50\";>" + stepDetails.get("keyword") + "</td><td>" + stepDetails.get("name") + "</td><td ");
                     }else {
-                        htmlTestReport.add("<td></td><td></td><td>" + bar.get("keyword") + "</td><td>" + bar.get("name") + "</td><td ");
+                        htmlTestReport.add("<td></td><td></td><td>"+(i+1)+"</td><td style=\" color: #4CAF50\";>" + stepDetails.get("keyword") + "</td><td>" + stepDetails.get("name") + "</td><td ");
                     }
-                    if(status.get("status").equals("passed")){htmlTestReport.add("style=\"background-color:Green;\"");
-                    }else if(status.get("status").equals("failed")){htmlTestReport.add("style=\"background-color:Red;\"");
-                    }else {htmlTestReport.add("style=\"background-color:Grey;\"");}
+                    if(status.get("status").equals("passed")){htmlTestReport.add("class=\"btn success\"");
+                    }else if(status.get("status").equals("failed")){htmlTestReport.add("class=\"btn danger\"");
+                    }else {htmlTestReport.add("class=\"btn default\"");}
 
-                    htmlTestReport.add(">"+status.get("status")+"</td><td>"+status.get("error_message")+"</td>");
+                    htmlTestReport.add(">"+toUpperCase((String) status.get("status"))+"</td>");
+                    if(status.get("status").equals("failed")) {
+                        htmlTestReport.add("<td style=\"border:1px solid Tomato; color: #f44336;\">" + status.get("error_message") + "</td>");
+                    }else if(status.get("status").equals("passed")){
+                        htmlTestReport.add("<td style=\"border: 1px solid lightgreen; color: #4CAF50;\">Working as Expected</td>");
+                    }else{
+                        htmlTestReport.add("<td style=\"border: 1px solid grey; color: black;\">Test Skipped</td>");
+                    }
                     htmlTestReport.add(Break());
                 }
 
@@ -93,6 +100,20 @@ public class JasonToHtmlReprot {
                 "th{\n" +
                 "    background-color: powderblue;\n" +
                 "}\n" +
+                ".success {background-color: #4CAF50; color: white;}\n" +
+                ".success:hover {background-color: #46a049; color: white}\n" +
+                "\n" +
+                ".info {background-color: #2196F3;}\n" +
+                ".info:hover {background: #0b7dda;}\n" +
+                "\n" +
+                ".warning {background-color: #ff9800;}\n" +
+                ".warning:hover {background: #e68a00;}\n" +
+                "\n" +
+                ".danger {background-color: #f44336; color: white;} \n" +
+                ".danger:hover {background: #da190b; color: white}\n" +
+                "\n" +
+                ".default {background-color: #e7e7e7; color: black;} \n" +
+                ".default:hover {background: #ddd;}\n"+
                 "</style>\n" +
                 "</head>\n" +
                 "<body>\n" +
@@ -101,9 +122,10 @@ public class JasonToHtmlReprot {
                 "    <th>Feature</th>\n" +
                 "    <th>Scenario</th>\n" +
                 "    <th>Step</th>\n" +
+                "    <th>Keyword</th>\n" +
                 "    <th>Step Details</th>\n" +
                 "\t<th>Status</th>\n" +
-                "\t<th>Failing Reason</th>\n" +
+                "\t<th>Comments</th>\n" +
                 "  </tr>\n" +
                 "  <tr>\n";
         return header;
@@ -127,4 +149,14 @@ public class JasonToHtmlReprot {
                 "    <tr>";
         return htmlBreak;
     }
+
+    public static  String toUpperCase(String str){
+
+        str = str.substring(0, 1).toUpperCase() + str.substring(1);
+
+        return str;
+    }
+
+
+
 }
